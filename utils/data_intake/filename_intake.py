98d435871a105bfae89b0032c31d5eb353e03279
Majid <majidkouki3301@gmail.com>
2022-08-07 13:27:50 -0700

    Created filename function.

--- utils/data_intake/filename_intake.py
+++ utils/data_intake/filename_intake.py
@@ -1,2 +1,14 @@
 # Import initial libraries and dependencies
 import questionary
+
+# Create a function that asks user for filename to save charts as
+def filename_intake():
+    print("\n")
+    print("And finally, what would you like the filenames to be? Duplicate names will override old ones and all files will be saved to included 'imgs' folder. Default is 1 & 2.")
+    print("\n")
+
+    # USe given name for name variable, else default to 1 & 2
+    name = questionary.text("Filename:").ask()
+
+    # Return name for file saving usage
+    return name
