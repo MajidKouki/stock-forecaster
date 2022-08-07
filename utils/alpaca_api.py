214919638233be16742d1823d5a54266e6faad75
Majid <majidkouki3301@gmail.com>
2022-08-07 16:07:14 -0700

    Added Alpaca API code and data prep.

--- utils/alpaca_api.py
+++ utils/alpaca_api.py
@@ -1,3 +1,36 @@
 # Import initial libraries and dependencies
 from dotenv import load_dotenv
 import alpaca_trade_api as tradeapi
+import pandas as pd
+import os
+
+# Create a function to pull data from Alpaca API and prepare it for use by Prophet
+def alpaca_api(tickers, timeframe, start_date, end_date):
+    # Load the environment variables from the .env file
+    load_dotenv()
+
+    # Set the variables for the Alpaca API key and secret key
+    alpaca_api_key = os.getenv("ALPACA_API_KEY")
+    alpaca_secret_key = os.getenv("ALPACA_SECRET_KEY")
+
+    # Create the Alpaca API REST object
+    alpaca = tradeapi.REST(
+        alpaca_api_key,
+        alpaca_secret_key,
+        api_version = "V2"
+    )
+
+
+    # Create dataframe using API data
+    data_df = alpaca.get_bars(
+        tickers,
+        timeframe,
+        start = start_date,
+        end = end_date,
+    ).df
+
+    # Drop columns not needed for Prophet usage
+    data_df = data_df.drop(["open", "high", "low", "volume", "trade_count", "vwap"], axis=1)
+
+    # Return dataframe for Prophet
+    return data_df
