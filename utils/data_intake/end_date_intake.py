f75c271023cd19e5eda9278c1cc5561dc499b33d
Majid <majidkouki3301@gmail.com>
2022-08-07 13:24:00 -0700

    Created date functions.

--- utils/data_intake/end_date_intake.py
+++ utils/data_intake/end_date_intake.py
@@ -1,2 +1,24 @@
 # Import initial libraries and dependencies
+from datetime import date
 import questionary
+import pandas as pd
+
+# Create a function that asks user for data end date and defaults to the current date if nothing is given
+def end_date_intake():
+    print("\n")
+
+    # Ask user to input a date using required format
+    end = questionary.text("Enter the end date using following format:'1234-56-78.' Default is current date.").ask()
+
+    # If nothing is entered, use default date
+    if end == "":
+        end = str(date.today())
+        print(f"Default Date: {end}")
+    else:
+        print(f"Selected Date: {end}")
+
+    # Ensure end date is formatted correctly for Alpaca API
+    end_date = pd.Timestamp(end, tz="America/New_York").isoformat()
+
+    # Return end date for Alpaca API
+    return end_date
--- utils/data_intake/start_date_intake.py
+++ utils/data_intake/start_date_intake.py
@@ -1,2 +1,23 @@
 # Import initial libraries and dependencies
 import questionary
+import pandas as pd
+
+# Create a function that asks user for data start date and defaults to the start of 2020 if nothing is given
+def start_date_intake():
+    print("\n")
+
+    # Ask user to input a date using required format
+    start = questionary.text("Enter the start date using following format:'1234-56-78.' Default is start of 2018.").ask()
+
+    # If nothing is entered, use default date
+    if start == "":
+        start = "2020-01-01"
+        print(f"Default Date: {start}")
+    else:
+        print(f"Selected Date: {start}")
+
+    # Ensure start date is formatted correctly for Alpaca API
+    start_date = pd.Timestamp(start, tz="America/New_York").isoformat()
+
+    # Return start date for Alpaca API
+    return start_date
