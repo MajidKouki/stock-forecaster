09ff914c98c099d3e989694ef37c3fa309123e12
Majid <majidkouki3301@gmail.com>
2022-08-07 08:16:40 -0700

    Finished financial disclaimer.

--- utils/financial_disclaimer.py
+++ utils/financial_disclaimer.py
@@ -1,2 +1,13 @@
 # Import initial libraries and dependencies
 import questionary
+import sys
+
+# Create a function that displays a financial disclaimer when the program is run and requires user to agree before proceeding
+def financial_disclaimer():
+    print("By using this application, you acknowledge that this is NOT financial advice and is merely a tool. \nThe creator of this program is not liable for any negative consequences that may occur from using it or the data it provides. \nContinue at your own risk.")
+    print("\n")
+    disclaimer = questionary.confirm("Do you agree to the above terms?").ask()
+    if disclaimer == True:
+        pass
+    else:
+        sys.exit("Sorry, you must agree to the above disclaimer in order to use this program.")
