2985a2a1d05d813d36fe7bcca998ca6a8981dd45
Majid <majidkouki3301@gmail.com>
2022-08-10 13:08:21 -0700

    Created signals function.

---
+++ utils/technical_signals.py
@@ -0,0 +1,6 @@
+# Import initial libraries and dependencies
+import pandas as pd
+import pandas_ta as ta
+
+# Define a function that uses closing prices and pandas ta to form a basic trading strategy and place buy and sell signals on a chart
+def technical_signals(data_df):
