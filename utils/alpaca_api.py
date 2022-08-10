909ad6498a0908a942c090f75447367efe984b89
Majid <majidkouki3301@gmail.com>
2022-08-10 12:28:36 -0700

    Added crypto specific functions.

--- utils/alpaca_api.py
+++ utils/alpaca_api.py
@@ -4,6 +4,7 @@ import alpaca_trade_api as tradeapi
 import pandas as pd
 import os
 
+
 # Create a function to pull data from Alpaca API and prepare it for use by Prophet
 def alpaca_api(tickers, timeframe, start_date, end_date):
     # Load the environment variables from the .env file
@@ -20,7 +21,6 @@ def alpaca_api(tickers, timeframe, start_date, end_date):
         api_version = "V2"
     )
 
-
     # Create dataframe using API data
     data_df = alpaca.get_bars(
         tickers,
@@ -28,6 +28,40 @@ def alpaca_api(tickers, timeframe, start_date, end_date):
         start = start_date,
         end = end_date,
     ).df
+
+    # Drop columns not needed for Prophet usage
+    data_df = data_df.drop(["open", "high", "low", "volume", "trade_count", "vwap"], axis=1)
+
+    # Return dataframe for Prophet
+    return data_df
+
+
+def alpaca_api_crypto(tickers, timeframe, start_date, end_date):
+    # Load the environment variables from the .env file
+    load_dotenv()
+
+    # Set the variables for the Alpaca API key and secret key
+    alpaca_api_key = os.getenv("ALPACA_API_KEY")
+    alpaca_secret_key = os.getenv("ALPACA_SECRET_KEY")
+
+    # Create the Alpaca API REST object
+    alpaca = tradeapi.REST(
+        alpaca_api_key,
+        alpaca_secret_key,
+        api_version = "V2"
+    )
+
+    # Create dataframe using API data
+    data_df = alpaca.get_crypto_bars(
+        tickers,
+        timeframe,
+        start = start_date,
+        end = end_date,
+    ).df
+
+    # Filter out all exchanges other than Coinbase and drop redundant exchange column
+    data_df = data_df[data_df.exchange == "CBSE"]
+    data_df = data_df.drop(axis=1, columns="exchange")
 
     # Drop columns not needed for Prophet usage
     data_df = data_df.drop(["open", "high", "low", "volume", "trade_count", "vwap"], axis=1)
--- utils/data_intake/ticker_intake.py
+++ utils/data_intake/ticker_intake.py
@@ -6,7 +6,26 @@ import sys
 def ticker_intake():
     print("\n")
 
-    tickers = questionary.text("What stock ticker would you like to forecast?").ask()
+    tickers = questionary.text("What stock ticker would you like to forecast? Ex. SPY").ask()
+
+    # If no ticker is given, sys.exit
+    if tickers == "":
+        sys.exit("Please try again using a valid ticker.")
+    else:
+        pass
+
+    # If ticker is given, use .upper() to ensure its properly formatted for API
+    tickers = tickers.upper()
+
+    # Return ticker for later use
+    return tickers
+
+
+# Create a function that asks user for ticker data for use with Alpaca API. Ensure ticker is formatted properly by converting to upper case
+def ticker_intake_crypto():
+    print("\n")
+
+    tickers = questionary.text("What stock ticker would you like to forecast? Ex. BTCUSD").ask()
 
     # If no ticker is given, sys.exit
     if tickers == "":
