b49a1e6101863f697a30956b4995efba081e981c
Majid <majidkouki3301@gmail.com>
2022-08-10 12:35:52 -0700

    Cleared old comments and added new comments to fit new code.

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from utils.data_intake.filename_intake import filename_intake
 from utils.data_intake.prophet_periods import prophet_periods
 
 
+# Forecast code using Alpaca get_bars for stock data
 def stock_forecast():
     # Intake ticker data from user and assign to tickers
     tickers = ticker_intake()
@@ -46,6 +47,8 @@ def stock_forecast():
     trends_plot.savefig(f"./imgs/{name}Trends.png")
     # indicator_plot.savefig(f"./imgs/{name}Indicators.png")
 
+
+# Forecast code using Alpaca get_crypto_bars for crypto data
 def crypto_forecast():
     # Intake ticker data from user and assign to tickers
     tickers = ticker_intake_crypto()
@@ -77,66 +80,20 @@ def crypto_forecast():
     # indicator_plot.savefig(f"./imgs/{name}Indicators.png")
 
 
-
 # Define function to house the program for use with Fire library
 def run():
     # Display financial disclaimer before running the rest of the program
     financial_disclaimer()
 
-    # If Else for crypto or stocks and forecast or indicators (maybe both?)
-    # Create broker for crypto get bars stuff. Create second API caller for crypto
-    # Create indicator option or automatically include. Options for indicators
-    # pandas TA
-
+    # Determine is crypto or stock data will be used with API
     crypto_stock = crypto_or_stock()
     
+    # Run stock version of code or crypto version of code based on answer above
     if crypto_stock == "Stocks":
         stock_forecast()
     elif crypto_stock == "Cryptocurrency":
         crypto_forecast()
 
-
-    # # Intake ticker data from user and assign to tickers
-    # tickers = ticker_intake()
-
-    # # Intake timeframe data from user and asign it to timeframe while creating frequency and tf
-    # timeframe, frequency, tf = timeframe_intake()
-
-    # # Ask user for forecast length data and use timeframe and tf to ensure program will run without errors
-    # length = prophet_periods(timeframe, tf)
-
-    # # Intake start date from user and assign to start_date
-    # start_date = start_date_intake()
-
-    # # Intake end date from user and assign to end_date
-    # end_date = end_date_intake()
-
-    # # Intake filename data from user and assign to name
-    # name = filename_intake(tickers, length, tf)
-
-
-    # # Use Alpaca API to create data_df dataframe for Prophet
-    # data_df = alpaca_api(tickers, timeframe, start_date, end_date)
-
-    # # Use Prophet to intake data from previous functions and produce a forecast plot and a trend plot
-    # data_plot, trends_plot = prophet_forecast(data_df, length, frequency)
-
-    # Save Prophet plots to 'imgs' folder using chosen filenames
-    # data_plot.savefig(f"./imgs/{name}Forecast.png")
-    # trends_plot.savefig(f"./imgs/{name}Trends.png")
-    # indicator_plot.savefig(f"./imgs/{name}Indicators.png")
-
 # Run the program
 if __name__ == "__main__":
     fire.Fire(run)
-
-
-
-"""
-    if A == True:
-        qowrnoqwnr
-    Elif B == True:
-        osegowhgoih
-    Else:
-        Sys yeet
-"""
