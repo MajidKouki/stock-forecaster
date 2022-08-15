bf81e80864c49238857820d85090d082d039ffa5
Majid <majidkouki3301@gmail.com>
2022-08-15 06:46:03 -0700

    Updated and simplified code structure.

--- app.py
+++ app.py
@@ -18,76 +18,6 @@ from utils.data_intake.filename_intake import filename_intake
 from utils.data_intake.prophet_periods import prophet_periods
 
 
-# Forecast code using Alpaca get_bars for stock data
-def stock_forecast():
-    # Intake ticker data from user and assign to tickers
-    tickers = ticker_intake()
-
-    # Intake timeframe data from user and asign it to timeframe while creating frequency and tf
-    timeframe, frequency, tf = timeframe_intake()
-
-    # Ask user for forecast length data and use timeframe and tf to ensure program will run without errors
-    length = prophet_periods(timeframe, tf)
-
-    # Intake start date from user and assign to start_date
-    start_date = start_date_intake()
-
-    # Intake end date from user and assign to end_date
-    end_date = end_date_intake()
-
-    # Intake filename data from user and assign to name
-    name = filename_intake(tickers, length, tf)
-
-    # Use Alpaca API to create data_df dataframe for Prophet
-    data_df = alpaca_api(tickers, timeframe, start_date, end_date)
-
-    # Use Prophet to intake data from previous functions and produce a forecast plot and a trend plot
-    data_plot, trends_plot = prophet_forecast(data_df, length, frequency)
-
-    # Use Pandas TA to identify buy and sell signals
-    signals_plot = technical_signals(data_df)
-
-    # Save Prophet plots to 'imgs' folder using chosen filenames
-    data_plot.savefig(f"./imgs/{name}Forecast.png", dpi=300)
-    trends_plot.savefig(f"./imgs/{name}Trends.png", dpi=300)
-    signals_plot.savefig(f"./imgs/{name}Signals.png", dpi=300)
-
-
-# Forecast code using Alpaca get_crypto_bars for crypto data
-def crypto_forecast():
-    # Intake ticker data from user and assign to tickers
-    tickers = ticker_intake_crypto()
-
-    # Intake timeframe data from user and asign it to timeframe while creating frequency and tf
-    timeframe, frequency, tf = timeframe_intake()
-
-    # Ask user for forecast length data and use timeframe and tf to ensure program will run without errors
-    length = prophet_periods(timeframe, tf)
-
-    # Intake start date from user and assign to start_date
-    start_date = start_date_intake()
-
-    # Intake end date from user and assign to end_date
-    end_date = end_date_intake()
-
-    # Intake filename data from user and assign to name
-    name = filename_intake(tickers, length, tf)
-
-    # Use Alpaca API to create data_df dataframe for Prophet
-    data_df = alpaca_api_crypto(tickers, timeframe, start_date, end_date)
-
-    # Use Prophet to intake data from previous functions and produce a forecast plot and a trend plot
-    data_plot, trends_plot = prophet_forecast(data_df, length, frequency)
-
-    # Use Pandas TA to identify buy and sell signals
-    signals_plot = technical_signals(data_df)
-
-    # Save Prophet plots to 'imgs' folder using chosen filenames
-    data_plot.savefig(f"./imgs/{name}Forecast.png", dpi=300)
-    trends_plot.savefig(f"./imgs/{name}Trends.png", dpi=300)
-    signals_plot.savefig(f"./imgs/{name}Signals.png", dpi=300)
-
-
 # Define function to house the program for use with Fire library
 def run():
     # Display financial disclaimer before running the rest of the program
@@ -96,11 +26,43 @@ def run():
     # Determine is crypto or stock data will be used with API
     crypto_stock = crypto_or_stock()
     
-    # Run stock version of code or crypto version of code based on answer above
+    # Run either stock or crypto version of ticker intake based on above reply
     if crypto_stock == "Stocks":
-        stock_forecast()
+        tickers = ticker_intake()
     elif crypto_stock == "Cryptocurrency":
-        crypto_forecast()
+        tickers = ticker_intake_crypto()
+
+    # Intake timeframe data from user and asign it to timeframe while creating frequency and tf
+    timeframe, frequency, tf = timeframe_intake()
+
+    # Ask user for forecast length data and use timeframe and tf to ensure program will run without errors
+    length = prophet_periods(timeframe, tf)
+
+    # Intake start date from user and assign to start_date
+    start_date = start_date_intake()
+
+    # Intake end date from user and assign to end_date
+    end_date = end_date_intake()
+
+    # Intake filename data from user and assign to name
+    name = filename_intake(tickers, length, tf)
+
+    # Use Alpaca API to create data_df dataframe for Prophet
+    if crypto_stock == "Stocks":
+        data_df = alpaca_api(tickers, timeframe, start_date, end_date)
+    elif crypto_stock == "Cryptocurrency":
+        data_df = alpaca_api_crypto(tickers, timeframe, start_date, end_date)
+
+    # Use Prophet to intake data from previous functions and produce a forecast plot and a trend plot
+    data_plot, trends_plot = prophet_forecast(data_df, length, frequency)
+
+    # Use Pandas TA to identify buy and sell signals
+    signals_plot = technical_signals(data_df)
+
+    # Save Prophet plots to 'imgs' folder using chosen filenames
+    data_plot.savefig(f"./imgs/{name}Forecast.png", dpi=300)
+    trends_plot.savefig(f"./imgs/{name}Trends.png", dpi=300)
+    signals_plot.savefig(f"./imgs/{name}Signals.png", dpi=300)
 
 
 # Run the program
