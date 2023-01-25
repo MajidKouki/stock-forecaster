e23a946ba2315de6f3d5b5a91b3aa8311c2d48f7
Majid <majidkouki3301@gmail.com>
2023-01-25 12:37:55 -0800

    Build testing functions in yahoo_api and further testing in yahoo_test

---
+++ utils/yahoo_api.py
@@ -0,0 +1,22 @@
+# Import initial libraries and dependencies
+import pandas as pd
+import yfinance as yf
+import streamlit as st
+
+
+
+
+# Create a function to pull data from Yahoo Finance API and prepare it for use by Prophet
+def yahoo_api_stock(tickers, period, interval):
+    # Create dataframe using API data
+    data_df = yf.download(
+        tickers = tickers, 
+        period = period, 
+        interval = interval
+    )
+
+    # Drop columns not needed for Prophet usage
+    data_df = data_df.drop(["Open", "High", "Low", "Adj Close", "Volume"], axis=1)
+
+    # Return dataframe for Prophet
+    return data_df
