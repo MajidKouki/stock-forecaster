8f845b1f7731aff7aa40f0dc13621ab7b768c1ea
Majid <majidkouki3301@gmail.com>
2023-04-06 10:43:09 -0700

    Added additional intake and indicators

--- utils/data_intake/technical_intake.py
+++ utils/data_intake/technical_intake.py
@@ -5,14 +5,23 @@ import pandas_ta as ta
 
 # Define a function to intake indicators for use in technical_signals
 def technical_intake():
+    # Prompt used to choose between available indicators
     indicator_1 = st.selectbox(
         "Select the first indicator",
-        ["EMA"]
+        ["EMA", "SMA"]
     )
-
     indicator_2 = st.selectbox(
         "Select the second indicator",
-        ["EMA"]
-    )
+        ["EMA", "SMA"]
+    )  
 
-    return indicator_1, indicator_2
+    # Intake the length for the indicators
+    indicator_1_len = st.text_input("Enter the length for the first indicator")
+    indicator_2_len = st.text_input("Enter the length for the second indicator")
+
+    # Ensure indicators are integers
+    indicator_1_len = int(indicator_1_len)
+    indicator_2_len = int(indicator_2_len)
+
+    # Return indicator info for use in charting
+    return indicator_1, indicator_2, indicator_1_len, indicator_2_len
