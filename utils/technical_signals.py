8562e83037c0d3256677a6ef0654bb20dcc6e803
Majid <majidkouki3301@gmail.com>
2023-04-06 10:51:19 -0700

    Added new indicators, reworked algorithm and legend

--- utils/technical_signals.py
+++ utils/technical_signals.py
@@ -5,17 +5,26 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 # Define a function that uses closing prices and pandas ta to form a basic trading strategy and place buy and sell signals on a chart
-def technical_signals(data_df):
-    # Define 14-day EMA and 28-day EMA
-    ema14 = ta.ema(data_df["Close"], length=14)
-    ema28 = ta.ema(data_df["Close"], length=28)
+def technical_signals(data_df, indicator_1, indicator_2, indicator_1_len, indicator_2_len):
+    # Check which indicator is chosen and setup info as needed
+    if indicator_1 == "EMA":
+        ind1 = ta.ema(data_df["Close"], length=indicator_1_len)
+    elif indicator_1 == "SMA":
+        ind1 = ta.sma(data_df["Close"], length=indicator_1_len)
+
+    if indicator_2 == "EMA":
+        ind2 = ta.ema(data_df["Close"], length=indicator_2_len)
+    elif indicator_1 == "SMA":
+        ind2 = ta.sma(data_df["Close"], length=indicator_2_len)
 
     # Convert EMAs to dataframes
-    ema14_df = pd.DataFrame(ema14)
-    ema28_df = pd.DataFrame(ema28)
+    ind1_df = pd.DataFrame(ind1)
+    ind2_df = pd.DataFrame(ind2)
 
     # Add EMA indicators to data_df dataframe
-    data_df = pd.concat([data_df, ema14_df, ema28_df], axis=1)
+    data_df = pd.concat([data_df, ind1_df, ind2_df], axis=1)
+
+    data_df.columns = ['Close','ind1','ind2']
 
     # Create signal lists and trigger for buy and sell signal loop
     buy_signals = []
@@ -25,12 +34,12 @@ def technical_signals(data_df):
     # Create loop to identify EMA crosses and create buy and sell signals at those points
     for x in range(len(data_df["Close"])):
         # If EMA_14 is above or at EMA_28, create a buy signal
-        if data_df["EMA_14"].iloc[x] >= data_df["EMA_28"].iloc[x] and trigger != 1:
+        if data_df["ind1"].iloc[x] >= data_df["ind2"].iloc[x] and trigger != 1:
             buy_signals.append(data_df["Close"].iloc[x])
             sell_signals.append(float("NaN"))
             trigger = 1
         # If EMA_14 is below or at EMA_28, create a sell signal
-        elif data_df["EMA_14"].iloc[x] <= data_df["EMA_28"].iloc[x] and trigger != -1:
+        elif data_df["ind1"].iloc[x] <= data_df["ind2"].iloc[x] and trigger != -1:
             buy_signals.append(float("NaN"))
             sell_signals.append(data_df["Close"].iloc[x])
             trigger = -1
@@ -50,12 +59,17 @@ def technical_signals(data_df):
     y2 = data_df["sell_signals"]
 
     signals_plot = plt.figure(figsize=(15, 10));
-    plt.scatter(x, y1, c='green', s=30, marker="v");
-    plt.scatter(x, y2, c='red', marker="^");
+
+    # Create legend labels based on indicator intake data
+    ind1_leg = str(indicator_1) + " " + str(indicator_1_len)
+    ind2_leg = str(indicator_2) + " " + str(indicator_2_len)
+
     data_df["Close"].plot();
-    data_df["EMA_14"].plot();
-    data_df["EMA_28"].plot();
-    plt.legend();
+    data_df["ind1"].plot();
+    data_df["ind2"].plot();
+    plt.scatter(x, y1, c='green', s=60, marker="v");
+    plt.scatter(x, y2, c='red', marker="^");
+    plt.legend(["Close", ind1_leg, ind2_leg]);
     plt.grid(True)
 
     return signals_plot
